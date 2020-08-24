c4f2787f38e799469d6801908d06ae94ba98a39a
Ing Oscar Lobo <oscarescalando@gmail.com>
2020-08-24 10:50:34 -0400

    test email

---
+++ resources/views/email/test.blade.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html lang="es">
+    <body>
+        <h1>¡Bienvenido a mi sitio !</h1>
+        <p>Espero que te diviertas en mi sitio y que invites a tus amigos a visitarlo :)</p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,11 @@ Route::group(['middleware' => ['cors']], function () {
     Route::get('{uuid}/default', 'WidgetController@show');
     Route::post('widget/data', 'WidgetDataController@store');
 });
+
+Route::get("/test_mail", function(Request $r){
+    $destinatario = "oscarescalando@gmail.com";
+    // Armar correo y pasarle datos para el constructor
+    $correo = new \App\Mail\TestMail();
+    # ¡Enviarlo!
+    \Mail::to($destinatario)->send($correo);
+});
