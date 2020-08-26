a10f71fc6badda22d452ff0b5a72452d56c42636
Ing Oscar Lobo <oscarescalando@gmail.com>
2020-08-26 19:33:12 -0400

    ajustes en plantilla y en js

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,10 @@
+<tr>
+<td class="header">
+
+<a href="https://dev.ventonic.com/widget/widgetsData" style="display: inline-block;">
+
+<img src="https://ventonic.com/images/logo-ventonic.png" class="logo" alt="Ventonic">
+
+</a>
+</td>
+</tr>
